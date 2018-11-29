af58f7df2b37eb76098d16f3d0734c33d1ad90ef
TheeBullhead <45467705+TheeBullhead@users.noreply.github.com>
2018-11-29 14:45:44 -0600

    CSS Calculator
    Cleaned up and removed unused conversion variables

--- css-calculator.cs
+++ css-calculator.cs
@@ -4,42 +4,35 @@ public class Program
 {
             public static void Main()
             {
-                        //Input 400 Swim Time in hh:mm:ss Format
-                        var inputMinutesFourHundred = 05d;//
-                        var inputSecondsFourHundred = 29d;//
-                        var inputMillisecondsFourHundred = .64d;//
+                        //Input 400 swim time in mm:ss.fff format
+                        var inputMinutesFourHundred = 05d;
+                        var inputSecondsFourHundred = 29d;
+                        var inputMillisecondsFourHundred = .64d;
                         
-                        //Input 200 Swim Time in hh:mm:ss Format
-                        var inputMinutesTwoHundred = 02d;//
-                        var inputSecondsTwoHundred = 42d;//
-                        var inputMillisecondsTwoHundred = .08d;//
+                        //Input 200 swim time in mm:ss.fff format
+                        var inputMinutesTwoHundred = 02d;
+                        var inputSecondsTwoHundred = 42d;
+                        var inputMillisecondsTwoHundred = .08d;
                         
-                        //Input Unit of Measurement Parameters (in whole units)
+                        //Input unit of measurement parameters
                         var inputUnitOfMeasurementSwam = "Yards";
-                        var inputDistanceSwam = 400d;//
-                        
-                        var inputUnitOfMeasurementPoolLength = "Yards";
                         var inputPoolLength = 25d;//
                         
-                        //Multipliers for Conversion to Seconds
+                        //Multipliers for conversion to seconds
                         const int divisorCss = 200;
                         const int secondsInMinute = 60;
                         const int unitOfMeasurementDistance = 100;
-                        
-                        //Placeholder for converted results
-                        var distanceSwam = inputDistanceSwam;
-                        var poolLength = inputPoolLength;
-                                    
-                        //Calculate Seconds Per 400 and 200 per Unit of Measure (Yards or Meters)
+
+                        //Calculate seconds per 400 and 200 per unit of measure (Yards or Meters)
                         var secondsPerFourHundredUnitOfMeasurement = ((inputMinutesFourHundred * secondsInMinute) + inputSecondsFourHundred + inputMillisecondsFourHundred);
                         var secondsPerTwoHundredUnitOfMeasurement = ((inputMinutesTwoHundred * secondsInMinute) + inputSecondsTwoHundred + inputMillisecondsTwoHundred);
                         var paceTimeCss = unitOfMeasurementDistance / (divisorCss / (secondsPerFourHundredUnitOfMeasurement - secondsPerTwoHundredUnitOfMeasurement));
                         
-                        //Convert Seconds Per 100 Unit of Measurement (Yards or Meters) Pace to mm:ss per 100 Yards
+                        //Convert seconds per 100 unit of measurement (Yards or Meters) pace to mm:ss.fff
                         var paceTimeCssPerHundred = TimeSpan.FromSeconds(paceTimeCss);
                         var paceTimeCssPerLength = TimeSpan.FromSeconds(paceTimeCss * (inputPoolLength / unitOfMeasurementDistance));
                         
                         Console.WriteLine($"Your CSS Pace (per 100 {inputUnitOfMeasurementSwam}): {paceTimeCssPerHundred.ToString(@"m\:ss\.fff")}");
-                        Console.WriteLine($"Your CSS Pace (per {inputPoolLength} {inputUnitOfMeasurementPoolLength}): {paceTimeCssPerLength.ToString(@"ss\.fff")}");
+                        Console.WriteLine($"Your CSS Pace (per {inputPoolLength} {inputUnitOfMeasurementSwam}): {paceTimeCssPerLength.ToString(@"ss\.fff")}");
             }
 }
