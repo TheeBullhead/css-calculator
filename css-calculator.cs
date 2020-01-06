6d562dbd87030f3f3609528671cc4fed70ad09f9
TheeBullhead <45467705+TheeBullhead@users.noreply.github.com>
2020-01-06 14:57:36 -0600

    Updated to include finish time
    Updated to include finish time calculated from CSS pace

--- css-calculator.cs
+++ css-calculator.cs
@@ -6,17 +6,17 @@ public class Program
     {
         //Input 400 Swim Time in hh:mm:ss Format
         var inputMinutesFourHundred = 05d;//
-        var inputSecondsFourHundred = 41d;//
+        var inputSecondsFourHundred = 21d;//
         var inputMillisecondsFourHundred = .33d;//
 
         //Input 200 Swim Time in hh:mm:ss Format
         var inputMinutesTwoHundred = 02d;//
-        var inputSecondsTwoHundred = 53d;//
+        var inputSecondsTwoHundred = 39d;//
         var inputMillisecondsTwoHundred = .00d;//
 
         //Input Unit of Measurement Parameters (in whole units)
         var inputUnitOfMeasurementSwam = "Yards";
-        var inputDistanceSwam = 400d;//
+        var inputDistanceSwam = 2112d;//
 
         var inputUnitOfMeasurementPoolLength = "Yards";
         var inputPoolLength = 25d;//
@@ -34,23 +34,19 @@ public class Program
         var secondsPerFourHundredUnitOfMeasurement = ((inputMinutesFourHundred * secondsInMinute) + inputSecondsFourHundred + inputMillisecondsFourHundred);
         var secondsPerTwoHundredUnitOfMeasurement = ((inputMinutesTwoHundred * secondsInMinute) + inputSecondsTwoHundred + inputMillisecondsTwoHundred);
         var paceTimeCss = unitOfMeasurementDistance / (divisorCss / (secondsPerFourHundredUnitOfMeasurement - secondsPerTwoHundredUnitOfMeasurement));
-
-        //Calculate Finish Times for 1 Mile and 1.2 Miles (Yards)
-        var secondsPerMile = paceTimeCss * 17.6;
-        var secondsPerOnePoint2Miles = paceTimeCss * 21.12;
-
+        
         //Convert Seconds Per 100 Unit of Measurement (Yards or Meters) Pace to mm:ss per 100 Yards
         var paceTimeCssPerHundred = TimeSpan.FromSeconds(paceTimeCss);
         var paceTimeCssPerLength = TimeSpan.FromSeconds(paceTimeCss * (inputPoolLength / unitOfMeasurementDistance));
 
-        //Convert Seconds Per Mile to mm:ss 
-        var mileFinishTime = TimeSpan.FromSeconds(secondsPerMile);
-        var milePointTwoFinishTime = TimeSpan.FromSeconds(secondsPerOnePoint2Miles);
-        
+        //Calculate Finish Times for Distance Swam (Yards)
+        var secondsPerDistanceSwam = (paceTimeCss * inputDistanceSwam) / unitOfMeasurementDistance;
+
+        //Convert Finish Time to mm:ss 
+        var finishTime = TimeSpan.FromSeconds(secondsPerDistanceSwam);
+
         Console.WriteLine($"Your CSS Pace (per 100 {inputUnitOfMeasurementSwam}): {paceTimeCssPerHundred.ToString(@"m\:ss\.fff")}");
         Console.WriteLine($"Your CSS Pace (per {inputPoolLength} {inputUnitOfMeasurementPoolLength}): {paceTimeCssPerLength.ToString(@"ss\.fff")}");
-
-        Console.WriteLine($"Your Finish Time for 1 Mile: {mileFinishTime.ToString(@"m\:ss\.fff")}");
-        Console.WriteLine($"Your Finish Time for 1.2 Miles: {milePointTwoFinishTime.ToString(@"m\:ss\.fff")}");
+        Console.WriteLine($"Your Finish Time for {inputUnitOfMeasurementSwam}: {finishTime.ToString(@"m\:ss\.fff")}");
     }
 }
